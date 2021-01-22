dad20789aafeea0c09d854258b4d7e85a1e68399
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2021-01-22 16:14:57 +0100

    Introducing Pipe Scriptable Object
    + added Pipe SO
    + added a custom editor for Pipe
    + fields of Pipe have restrictions (where it makes sense, mainly if maximumScore is lower than minimumScore it gets adjusted to the value of minimumScore)

---
+++ Assets/Resources/Pipes/Editor_Pipe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace FlappyBirdPlusPlus
+{
+    [CustomEditor(typeof(Pipe))]
+    public class Editior_Pipe : Editor
+    {        
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            Pipe pipe = (Pipe)target; // get access to fields
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("topPipeTexture"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("bottomPipeTexture"));
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("minimumScore"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("restrictMaximumScore"));
+            if (pipe.RestrictMaximumScore)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("maximumScore"));
+                if(pipe.MinimumScore > pipe.MaximumScore)
+                {
+                    pipe.MaximumScore = pipe.MinimumScore;
+                }
+            }
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("customSpawnWeight"));
+            if (pipe.CustomSpawnWeight)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("weight"));
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Resources/Pipes/Pipe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBirdPlusPlus
+{
+    [CreateAssetMenu(fileName = "Pipe_Default", menuName = "GameCustomization/Pipe")]
+    public class Pipe : ScriptableObject
+    {
+        [Header("Visuals")]
+        [SerializeField] [Tooltip("The texture of the upper pipe")]
+        private Texture2D topPipeTexture;
+        public Texture2D TopPipe { get => topPipeTexture; }
+        [SerializeField] [Tooltip("The texture of the lower pipe")]
+        private Texture2D bottomPipeTexture;
+        public Texture2D BottomPipe { get => bottomPipeTexture; }
+
+        [Header("Spawning")]
+        [SerializeField] [Tooltip("The lowest score that the player has to achieve so that this pipe can appear")] [Min(0)]
+        private int minimumScore = 0;
+        public int MinimumScore { get => minimumScore; }
+
+        [SerializeField] [Tooltip("Should there be an upper limit to the appearence of this pipe?")]
+        private bool restrictMaximumScore;
+        public bool RestrictMaximumScore { get => restrictMaximumScore; }
+        [SerializeField] [Tooltip("If the player achieves this score, the pipe will no longer appear")] [Min(0)]
+        private int maximumScore = int.MaxValue; // make this "tickable" (if not visible then set to "ininity")
+        public int MaximumScore { get => maximumScore; set => maximumScore = value; }
+
+        [SerializeField] [Tooltip("Should this pipe have custom chance of spawning?")]
+        private bool customSpawnWeight;
+        public bool CustomSpawnWeight { get => customSpawnWeight; }
+        [SerializeField] [Tooltip("How likely should this pipe configuration appear if there are many possible")] [Min(1)]
+        private int weight = 1; 
+        public int Weight { get => weight; }
+    }
+}
