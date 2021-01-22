31aaa83c550477a4c56a33fd73f1fb8180acd6c6
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2021-01-22 12:09:15 +0100

    GameScene initialization + config scriptable object
    + added the GameSceneInitializer (to inject dependencies)
    + added GameSettings : a ScriptableObject for designers to configure the game (for now it holds just the bird prefab)

---
+++ Assets/GameSettings/GameSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBirdPlusPlus
+{
+    [CreateAssetMenu]
+    public class GameSettings : ScriptableObject
+    {
+        [SerializeField] GameObject playerPrefab = null;
+        public GameObject PlayerPrefab { get => playerPrefab; }
+    }
+}
---
+++ Assets/Scripts/GameSceneInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBirdPlusPlus
+{
+    public class GameSceneInitializer : MonoBehaviour
+    {
+        [SerializeField] GameSettings gameSettings;
+        [SerializeField] Transform startingPosition;
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameObject playerGameObject = Instantiate(gameSettings.PlayerPrefab, startingPosition.position, startingPosition.rotation);
+            Destroy(startingPosition.gameObject);
+            // Initialize the GameplayManager (I guess just call "initialize") - the gameplay manager will "unpause" the bird when it is done doing stuff
+            // pass the playerGameObject to the GameplayManager so that it can have it
+        }
+    }
+}
