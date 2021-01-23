e33288e48ad3975d2d8e4bed5d08924898d71a5d
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2021-01-24 00:18:31 +0100

    Game Over finished + Highscore System
    + implemented a highscoreSystem using PlayerPrefs (as required by the task A)
    + polished the End Game screen animations
    + now the player knows if they got a highscore or not
    + added a button to play again

--- Assets/Scripts/GameUI/GameOverDisplay.cs
+++ Assets/Scripts/GameUI/GameOverDisplay.cs
@@ -8,26 +8,75 @@ namespace FlappyBirdPlusPlus
 {
     public class GameOverDisplay : MonoBehaviour
     {
-        [SerializeField] TMP_Text finalScoreText = null;
+        [SerializeField] Image backgroundImage = null;
         [SerializeField] GameObject boxGameObject = null;
+        [SerializeField] TMP_Text finalScoreText = null;
+        [SerializeField] TMP_Text highscoreText = null;
+        [SerializeField] RectTransform playAgainButton = null;
 
         public void Initialize(GameplayManager gameplayManager)
         {
             gameplayManager.announceEndGame += HandleShowEndGame;
             boxGameObject.transform.localPosition = new Vector3(0f, -Screen.height, 0);
             // set aplha of the background to 0 so that it can fade in
-            Image backgroundImage = GetComponent<Image>();
             Color temp = backgroundImage.color;
             temp.a = 0f;
             backgroundImage.color = temp;
+
+            highscoreText.rectTransform.localScale = Vector3.zero; // hide the information about the highscore
+            playAgainButton.localScale = Vector3.zero; // hide the button
         }
 
         private void HandleShowEndGame(int finalScore)
         {
-            LeanTween.alpha(GetComponent<Image>().rectTransform, 0.5f, 0.5f);
+            LeanTween.alpha(backgroundImage.rectTransform, 0.5f, 0.5f).setRecursive(false); // setRecursive(false) so we don't tween the alpha on other child images
             LeanTween.moveY(boxGameObject, Screen.height/2, 1f).setEase(LeanTweenType.easeOutBounce);            
             gameObject.SetActive(true);
             finalScoreText.text = finalScore.ToString();
+
+            CheckForHighscore(finalScore);
         }
+
+        private void CheckForHighscore(int finalScore)
+        {
+            List<int> highscores = SaveSystem.ReadHighscores();
+            bool hasImprovedHighscores = false;
+
+            for (int i = 0; i < highscores.Count; i++)
+            {
+                if (highscores[i] < finalScore)
+                {
+                    hasImprovedHighscores = true;
+                    highscores.Insert(i, finalScore); //add score
+                    if (highscores.Count > SaveSystem.HIGHSCORE_COUNT) // remove excess highscores (should be only 1 to remove but let's suppose that there is an update and the highscore count has been decreased
+                    {
+                        while (highscores.Count > SaveSystem.HIGHSCORE_COUNT)
+                        {
+                            highscores.RemoveAt(highscores.Count - 1);
+                        }
+                    }
+                    highscoreText.text = "Highscore #" + (i+1) + "!"; // inform the player which spot on the highscore list they have achieved
+                    
+                    i += highscores.Count;// exit out of the loop
+                }
+            }
+
+            SaveSystem.SaveHighscores(highscores); // save the new set of highscores
+
+            if (!hasImprovedHighscores) // if did not get a highscore, then give this message
+            {
+                highscoreText.text = "Better luck next time!";
+            }
+
+            LeanTween.scale(highscoreText.rectTransform, Vector3.one, 1f).setEase(LeanTweenType.easeOutBounce).setDelay(1f); ; // show whether we got the higscore or not
+            LeanTween.scale(playAgainButton, Vector3.one, 1f).setEase(LeanTweenType.easeOutBounce).setDelay(1.5f); ; // show whether we got the higscore or not
+        }
+
+        #region Buttons
+        public void ButtonStartAgain()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        }
+        #endregion
     }
 }
---
+++ Assets/Scripts/Tools/SaveSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public const int HIGHSCORE_COUNT = 5;
+
+    public static List<int> ReadHighscores() // Returns all saved highscores, sorted from highest to lowest
+    {
+        List<int> loadedScores = new List<int>();
+        for(int i = 0; i < HIGHSCORE_COUNT; i++)
+        {
+            string lookedUpPref = "HS" + i;
+            int loadedValue = 0;
+            if(PlayerPrefs.HasKey(lookedUpPref))
+            {
+                loadedValue = PlayerPrefs.GetInt(lookedUpPref);
+            }
+            loadedScores.Insert(i, loadedValue);
+        }
+
+        for(int i = 0; i < loadedScores.Count; i++)
+        {
+            Debug.Log("Loaded Highscore #" + i + " : " + loadedScores[i]);
+        }        
+
+        return loadedScores;
+    }
+
+    public static void SaveHighscores(List<int> scores) // Assumes that scores are sorted
+    {
+        for(int i = 0; i < scores.Count; i++)
+        {
+            PlayerPrefs.SetInt("HS" + i, scores[i]);
+        }
+
+        for (int i = 0; i < scores.Count; i++)
+        {
+            Debug.Log("Saved Highscore #" + i + " : " + scores[i]);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+}
