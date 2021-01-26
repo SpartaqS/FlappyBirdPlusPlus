fc88f6bf9afc3e4b239b3d07a7b61a6d96ed91c2
Gabriel Ksawery Skowron-Rodriguez <gsrodriguez@wp.pl>
2021-01-26 23:42:19 +0100

    Added unit tests for player graphics
    + three small tests that end up checking if the player prefab has a sprite assigned to it
    + rearranged the project structure a bit to allow unit testing:
    + moved the prefabs to the Resources folder (easier to test this way and all are loaded into the game anyways)
    + added an assembly  for LeanTween since it had none.

---
+++ Assets/Tests/EditMode/PlayerGraphicsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class PlayerGraphicsTests
+    {
+        [Test]
+        public void PlayerPrefabExists()
+        {
+            GameObject playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerBird");
+            Assert.AreNotEqual(null, playerPrefab);
+        }
+
+        [Test]
+        public void PrefabHasSpriteRenderer()
+        {
+            GameObject playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerBird");
+
+            SpriteRenderer playerSpriteRenderer = null;
+            Assert.IsTrue(playerPrefab.TryGetComponent<SpriteRenderer>(out playerSpriteRenderer));            
+        }
+
+        [Test]
+        public void PlayerGraphicsCorrectlyDefined()
+        {
+            GameObject playerPrefab = Resources.Load<GameObject>("Prefabs/PlayerBird");
+            SpriteRenderer playerSpriteRenderer = playerPrefab.GetComponent<SpriteRenderer>();
+
+            Assert.AreNotEqual(null, playerSpriteRenderer.sprite); // test if a sprite has been assigned            
+        }
+    }
+}
